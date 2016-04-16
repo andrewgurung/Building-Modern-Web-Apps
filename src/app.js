6443d29c295c249579ecaa9ab728f2a76bdb6153
andrewgurung <andrewgurungg@gmail.com>
2016-04-16 10:55:43 -0400

    Simple React component: Hello

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 var React = require('react')
+
+// Create simple Hello component
+var Hello = React.createClass({
+  render: function () {
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+// `.render(what_to_render, where_to_render)`
+React.render(<Hello name="JS 2016!"/>, document.body)
