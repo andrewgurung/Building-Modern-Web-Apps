79823adeee6c2c74c8b94746b8fa00b0a1495f2b
andrewgurung <andrewgurungg@gmail.com>
2016-04-16 17:51:31 -0400

    Add two routes: public and repos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
+import Router from './router'
+import styles from './styles/main.styl'
+
 window.app = {
   init() {
+    // Create a router instance
+    this.router = new Router()
 
+    // No matter how many router you instantiate
+    // there will be only one history object (Represents browser's history)
+    // .start() will trigger it
+    this.router.history.start()
   }
 }
 
--- src/router.js
+++ src/router.js
@@ -1,1 +1,18 @@
 import Router from 'ampersand-router'
+
+export default Router.extend({
+  routes: {
+    // route path : handler string 'public'--> function 'public(){..}'
+    // http://localhost:3000/ --> prints 'public page' on console
+    '': 'public',
+    'repos': 'repos'
+  },
+
+  public() {
+    console.log('public page')
+  },
+
+  repos() {
+    console.log('repos')
+  }
+})
